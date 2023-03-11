1dc2d1fbfce709c10ccb01302db759059b853522
AllahDalla <alromariodavis@gmail.com>
2023-03-10 19:38:32 -0500

    Link to admin page needs to be implemented. Fix taskboard table. add appropriate icons and logos

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -1,74 +1,153 @@
 import React from 'react'
 import '../css/admin.css'
+import { useNavigate } from 'react-router-dom';
 
 import NavBar from './navbar'
+import GmailIcon from '../images/gmail-icon.png'
 
 
 export default function Admin(){
     const [data, setData] = React.useState([]);
+    // const [currentUrl, setCurrentUrl] = React.useState(window.location.href);
+    // const currentUrl = window.location.href
 
-    const handleAddRow = () => {
-        setData([...data, {
-        platform: '',
-        subject: '',
-        snippet: ''
-        }]);
+    // const handleInputChange = (index, event) => {
+    //     const { name, value } = event.target;
+    //     const newData = [...data];
+    //     newData[index][name] = value;
+    //     setData(newData);
+    // }
+
+    const dummy_data = [
+        {
+            key:1,
+            platform:"Gmail",
+            headers:{name:"Subject", value:"Security alert"},
+            body:"No message!"
+        },
+        {
+            key:2,
+            platform:"Gmail",
+            headers:{name:"Subject", value:"\\\"NBA Players you didn't realize were related?\\\""},
+            body:"No message!"
+        },
+        {
+            key:3,
+            platform:"Gmail",
+            headers:{name:"Subject", value:"\\\"NBA Players you didn't realize were related?\\\""},
+            body:"I'm not scared of lions, tigers and bears, but I'm scared of....loving you. Wooohooo, yuh done know a regular tingdakdagdiakbdkhabdkbkabdkabkdbkabdhkbahdkhakhdbkjb aduuaiuhdoaud duabdabdi diabd iadbai di addoahodhaodnoad andandja d dada dadouandlq dwoahdoadbod dobekjd ek adoaedje haifn fdsdkls fshdf skfabfak fid adjedhwnfa  nadadbaidbiadibad auddbkbad"
+        }
+    ]
+
+    function handleAddingIcomingRequest(event){
+        const {name} = event.target
+        let updateTaskboardList = []
+        data.forEach((object) => {
+            // console.log(typeof(object.key) ,typeof(name))
+            if(object.key.toString() === name){
+            
+                async function UpdateTaskboard(updateObject){
+                    const url = 'http://localhost:7000/update-taskboard'
+                    const requestObject = {
+                        method:"POST",
+                        headers:{'Content-Type': 'application/json'},
+                        body:JSON.stringify(updateObject)
+                    }
+                    await fetch(url, requestObject)
+                        .then(async response => {
+                            if(!response.ok){
+                                // do nothing
+                                alert("An error occured trying to update taskboard!")
+                            }else{
+                                alert("Taskboard updated successfully!")
+                            }
+                        })
+
+                        .catch(async error => {
+                            console.log(error)
+                        })
+                }
+
+                UpdateTaskboard(object)
+                const updatedItems = data.filter((item) => item.key.toString() !== name);
+                setData(updatedItems)
+            }
+        })
     }
 
-    const handleInputChange = (index, event) => {
-        const { name, value } = event.target;
-        const newData = [...data];
-        newData[index][name] = value;
-        setData(newData);
-    }
-
-    async function handleIncomingClick(){
-        console.log("Clicked!")
+    async function getEmailMessages(){
         const Obj = {
             method:"POST",
             headers:{'Content-Type': 'text/html'},
             body:"Requesting platform requests"
         }
 
-        await fetch("/platform-call", Obj)
+        await fetch("http://localhost:7000/platform-call", Obj)
             .then(async response => {
-                let res = await response.text()
-                console.log(res)
+                if(!response.ok){
+                    //do nothing but alert user
+                    const alertUserString = `Sign in to Google first by going to /google`
+                    alert(alertUserString)
+                }else{
+
+                    let res = await response.text()
+                    let objects = res.trim().split('\n')
+                    for(let index = 0; index < objects.length; index++){
+                        let data = {
+                            key:JSON.parse(objects[index]).key,
+                            platform:JSON.parse(objects[index]).platform,
+                            subject:JSON.parse(objects[index]).headers.value,
+                            message:JSON.parse(objects[index]).body
+                        }
+    
+                        setData((prev) => (
+                            [...prev, data]
+                            ))
+                    }
+                   
+                }
+                   
+
             })
 
             .catch(async error => {
                 console.log(await error)
             })
     }
+    React.useEffect(()=>{
+        getEmailMessages()
+    },[])
     return(
         <>
             <NavBar />
-            <button onClick={handleIncomingClick}>Incoming</button>
+            {/* <button className='incoming-btn' onClick={handleIncomingClick}>Incoming</button> */}
             <div className='incoming-table-container'>
-                <table>
+                <table className='incoming-table'>
                     <thead>
                         <tr>
-                            <th>Platform</th>
-                            <th>Subject</th>
-                            <th>Snippet</th>
-                            <th>Add</th>
+                            <th className="table-cell">Platform</th>
+                            <th className="table-cell">Subject</th>
+                            <th className="table-cell">Snippet</th>
+                            <th className="table-cell">Add</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map((row, index) => (
-                            <tr key={index}>
-                            <td>
-                                <input type="text" name="platform" value={row.platform} onChange={e => handleInputChange(index, e)} />
-                            </td>
-                            <td>
-                                <input type="text" name="subject" value={row.subject} onChange={e => handleInputChange(index, e)} />
-                            </td>
-                            <td>
-                                <input type="text" name="snippet" value={row.snippet} onChange={e => handleInputChange(index, e)} />
-                            </td>
-                            <td>
-                                <button onClick={handleAddRow}>Add</button>
-                            </td>
+                        {data.map((element) => (
+                            <tr key={element.key}>
+                                <td className="table-cell">
+                                    <div className='platform-container'>
+                                        <img id='platform-icon' src={GmailIcon} alt="Gmail icon, a red and white envelope"/>{element.platform}
+                                    </div>
+                                </td>
+                                <td className="table-cell">
+                                    {element.subject}
+                                </td>
+                                <td className="table-cell">
+                                    {element.message}
+                                </td>
+                                <td className="table-cell" style={{paddingBottom:"20px"}}>
+                                    <button  name={element.key} onClick={handleAddingIcomingRequest}>Add</button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -10,6 +10,8 @@ export default function Table(){
     const month = date.getMonth() + 1
     const year = date.getFullYear()
 
+    const [tableInfo, setTableInfo] = React.useState({})
+
     const dummyRequests = {
         1:{
             query_name:"Scholarship1",
@@ -93,11 +95,46 @@ export default function Table(){
         },
 
     }
-    let requests = Object.keys(dummyRequests).map((keys) => (<Query query_name={dummyRequests[keys].query_name} dept={dummyRequests[keys].dept} sender_name={dummyRequests[keys].sender_name} date={dummyRequests[keys].date} issue={dummyRequests[keys].issue}/>))
-    // for (let x = 1; x == Object.keys(dummyRequests).length; x++){
-    //     let query =  <Query query_name={dummyRequests[x].query_name} dept={dummyRequests[x].dept} sender_name={dummyRequests[x].sender_name} date={dummyRequests[x].date} issue={dummyRequests[x].issue}/>
-    //     requests.push(query)
-    // }
+    React.useEffect(() => {
+        async function getTaskboard(){
+            const url = 'http://localhost:7000/populate'
+            await fetch(url)
+                .then(async response => {
+                    if(!response.ok){
+                        //do nothing but alert the user
+                        alert("Taskboard could not be updated!")
+                    }else{
+                        let file = await response.text()
+                        let tabledata = file.split('\n')
+                        // console.log(tabledata)
+                        // console.log(JSON.parse(tabledata[0]).subject)
+                        let updatedtabledata = {}
+                        for(let index = 0; index < tabledata.length; index++){
+                            if(tabledata[index] !== ''){
+                                let data = {
+                                    query_name:JSON.parse(tabledata[index]).subject,
+                                    dept:"Something for now",
+                                    sender_name:"Need to get that",
+                                    date:`${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`,
+                                    issue:"Unsolved"
+                                }
+                                updatedtabledata[index] = data
+                            }
+                        }
+                        console.log(updatedtabledata)
+                        setTableInfo(updatedtabledata)
+                    }
+                })
+
+                .catch(async err => {
+                    console.log(err)
+                })
+        }
+
+        getTaskboard()
+    }, [])
+    let requests = Object.keys(tableInfo).map((keys) => (<Query key={tableInfo[keys]} query_name={tableInfo[keys].query_name} dept={tableInfo[keys].dept} sender_name={tableInfo[keys].sender_name} date={tableInfo[keys].date} issue={tableInfo[keys].issue}/>))
+    
 
     return(
         <div className='table'>
